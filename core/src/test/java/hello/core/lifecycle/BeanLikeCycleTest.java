cf88a97d39b7fe5190e6da7d58927b2c86b4fd80
Geomgi <cgg7777@naver.com>
2023-06-13 11:18:36 +0900

    Call back method (initMethod)

--- core/src/test/java/hello/core/lifecycle/BeanLikeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLikeCycleTest.java
@@ -17,7 +17,7 @@ public class BeanLikeCycleTest {
 
     @Configuration
     static class LifeCycleConfig{
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.day");
